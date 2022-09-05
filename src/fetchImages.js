bf746d1cc48aeab7f85a20e1e0c89b73a0443090
Vasyl Tarasyuk <vasfreelan@gmail.com>
2022-09-05 23:20:15 +0300

    6

--- src/fetchImages.js
+++ src/fetchImages.js
@@ -5,5 +5,6 @@ const KEY = '15486639-fb36d9e164edb7a2c5eb45855';
 
 export default async function fetchImages(input, page, perPage) {
   const completeUrl = `?key=${KEY}&q=${input}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`;
-  return (response = await axios.get(completeUrl));
+  const response = await axios.get(completeUrl);
+  return response;
 }
