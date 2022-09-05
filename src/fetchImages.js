40026722d80f62b1239a299e6a4a37dcd80adc09
Vasyl Tarasyuk <vasfreelan@gmail.com>
2022-09-05 22:46:19 +0300

    4

--- src/fetchImages.js
+++ src/fetchImages.js
@@ -1,9 +1,11 @@
 import axios from 'axios';
 
+export { fetchImages };
+
 axios.defaults.baseURL = 'https://pixabay.com/api/';
 const KEY = '15486639-fb36d9e164edb7a2c5eb45855';
 
-export async function fetchImages(input, page, perPage) {
+async function fetchImages(input, page, perPage) {
   const completeUrl = `?key=${KEY}&q=${input}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`;
   return (response = await axios.get(completeUrl));
 }
